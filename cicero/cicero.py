6e7572f2163ade14bce98ec697668aa34f14ccfd
Justin Huynh <polar.truth@gmail.com>
2018-09-08 13:23:39 -0400

    Added count for most common noun phrases

--- cicero/cicero.py
+++ cicero/cicero.py
@@ -1,8 +1,10 @@
 from textblob import TextBlob, Word
+import textblob
 from textblob.wordnet import VERB
 import praw
 from praw.models import MoreComments
 import pandas as pd
+from collections import Counter
 
 
 def m1():
@@ -23,10 +25,17 @@ def most_used_word(phrase):
         print(text.words.count(word))
 
 
-def subreddit_scrape(reddit):
-    subreddit = reddit.subreddit('vancouver')
+def subreddit_scrape(reddit, subreddit_name):
+    """
+    Scrapes the subredits within subreddit_list
+    :param reddit: Reddit object used to connect
+    :param subreddit_name: Name of subreddit to scrape
+    :return: Submissions and comments
+    """
+    subreddit = reddit.subreddit(subreddit_name)
 
-    top_subreddit = subreddit.top(limit=100)
+    top_subreddit = subreddit.top(limit=10,
+                                  time_filter='year')
     # hot_subreddit = subreddit.hot(limit=10)
 
     topics_dict = {"title": [],
@@ -38,11 +47,22 @@ def subreddit_scrape(reddit):
                    "body": [],
                    "comments": []}
 
+    comments_sentiment = {"comment": [],
+                          "sentiment": []}
+
     for submission in top_subreddit:
         submission.comments.replace_more(limit=None)
         for top_level_comment in submission.comments:
-            print(top_level_comment.body)
+            text = TextBlob(top_level_comment.body)
+            polarity = text.sentiment.polarity
+            comments_sentiment["comment"].append(top_level_comment.body)
+            comments_sentiment["sentiment"].append(polarity)
 
+    data = pd.DataFrame(comments_sentiment)
+    print(data.describe())
+    tblob = TextBlob(str(comments_sentiment["comment"]))
+    cnt = Counter(str(tblob.noun_phrases).split()).most_common(100)
+    print(cnt)
     # for submission in top_subreddit:
     #     topics_dict["title"].append(submission.title)
     #     topics_dict["score"].append(submission.score)
@@ -57,7 +77,6 @@ def subreddit_scrape(reddit):
     # topics_data = pd.DataFrame(topics_dict)
 
 
-
 def get_comments(reddit):
     submission = reddit.submission(url='https://www.reddit.com/r/funny/comments/3g1jfi/buttons/')
     # or with the submission’s ID which comes after comments / in the URL:
@@ -67,14 +86,23 @@ def get_comments(reddit):
     for top_level_comment in submission.comments:
         print(top_level_comment.body)
 
+def get_sentiment(text):
+    pass
+
+def get_frequency(text):
+    pass
+
+
 if __name__ == '__main__':
     reddit = praw.Reddit(client_id='XXXX',
                          client_secret='XXXX',
-                         user_agent='XXXX',
-                         username='XXXX',
-                         password='XXXX')
+                         user_agent='XXXX')
+    cities = ["ottawa", "toronto", "calgary", "vancouver", "HongKong"]
 
-    subreddit_scrape(reddit)
+    for city in cities:
+        print(city)
+        subreddit_scrape(reddit, city)
+
     # get_comments(reddit)
 
 
